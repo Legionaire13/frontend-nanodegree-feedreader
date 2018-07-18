8109965e224fa07aca69c39bc7cec34ef16a7dfa
legionaire13 <legionaire13@mail.ru>
2018-07-19 01:31:55 +0300

    checking url's and names are defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('have every url defined', function() {
+            for (var feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).toContain("http://");
+                expect(feed.url).not.toBe("");
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
